7ebfc8fa55370fbccd2e563a74c2062ae09a425b
jferreira <jose.ferreira@kiman.com.br>
2025-04-27 21:50:20 -0300

    Adding project CryptoMonitor for learn how use external apis

---
+++ android-kotlin/android_crypto_monitor/app/src/main/java/soraaslon/com/github/android_crypto_monitor/service/MercadoBitcoinService.kt
@@ -0,0 +1,11 @@
+package soraaslon.com.github.android_crypto_monitor.service
+
+import retrofit2.Response
+import retrofit2.http.GET
+import soraaslon.com.github.android_crypto_monitor.model.TickerResponse
+
+interface MercadoBitcoinService {
+
+    @GET("api/BTC/ticker/")
+    suspend fun getTicker(): Response<TickerResponse>
+}
